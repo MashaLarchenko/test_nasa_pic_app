4584ca30df12514d23e317095b6c84e4afc98876
maria larchenko <mashalarchenko16@gmail.com>
2020-06-18 18:43:48 +0300

    Updates

---
+++ precache-manifest.1fbdf2eac3d468bb2262d97702d8931d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b743b1ff252197cd70e1a9445ddaa846",
+    "url": "/pictureOfTheDay/index.html"
+  },
+  {
+    "revision": "142aeaa55b9512463246",
+    "url": "/pictureOfTheDay/static/css/main.f7bd070a.chunk.css"
+  },
+  {
+    "revision": "2b5a615ce79d4d4581d4",
+    "url": "/pictureOfTheDay/static/js/2.9f7e2e03.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pictureOfTheDay/static/js/2.9f7e2e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142aeaa55b9512463246",
+    "url": "/pictureOfTheDay/static/js/main.57fbcc3b.chunk.js"
+  },
+  {
+    "revision": "23ff371b6f1fb709dc92",
+    "url": "/pictureOfTheDay/static/js/runtime-main.66f10310.js"
+  },
+  {
+    "revision": "5edbeeb90394792e34c6458e2ec767ca",
+    "url": "/pictureOfTheDay/static/media/1.5edbeeb9.jpg"
+  },
+  {
+    "revision": "60c22a8c0c70f2e3ef36da3a9c201c09",
+    "url": "/pictureOfTheDay/static/media/2.60c22a8c.jpg"
+  },
+  {
+    "revision": "04e2636f6619398c6fc0ecd0745b334f",
+    "url": "/pictureOfTheDay/static/media/3.04e2636f.jpg"
+  },
+  {
+    "revision": "e1a1e0dc24ecd8b793b1b351436d52b4",
+    "url": "/pictureOfTheDay/static/media/4.e1a1e0dc.jpg"
+  },
+  {
+    "revision": "6b8ae61793010ef87d734f519bd178c6",
+    "url": "/pictureOfTheDay/static/media/5.6b8ae617.jpg"
+  },
+  {
+    "revision": "ea98f6ba93597b4f6345f38a4f5298cb",
+    "url": "/pictureOfTheDay/static/media/7.ea98f6ba.jpg"
+  },
+  {
+    "revision": "60bdfe87e342f4f674f039e7ee468a40",
+    "url": "/pictureOfTheDay/static/media/8.60bdfe87.jpg"
+  },
+  {
+    "revision": "93548749d24705bc1a73a61b74bee922",
+    "url": "/pictureOfTheDay/static/media/9.93548749.jpg"
+  },
+  {
+    "revision": "2c012d6c02c810b474f6d41790b3244d",
+    "url": "/pictureOfTheDay/static/media/MilkyWay.2c012d6c.mp4"
+  }
+]);
