157da5572bb034767f0184ce2c1385e0d0aef41c
maria larchenko <mashalarchenko16@gmail.com>
2020-06-19 17:19:05 +0300

    fix: update link in navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,10 @@ export const Navigation = () => {
         <nav className="nav-wrapper">
             <span className="nav_title">Astronomy Picture of the Day</span>
             <ul id="nav-mobile" className="nav_list">
-                <li><NavLink to='/calendar' onClick={() => {
+                <li><NavLink to='/test_nasa_pic_app/calendar' onClick={() => {
                    refreshPage()
                 }}>Calendar</NavLink></li>
-                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/test_nasa_pic_app/">Home</NavLink></li>
             </ul>
         </nav>
     )
