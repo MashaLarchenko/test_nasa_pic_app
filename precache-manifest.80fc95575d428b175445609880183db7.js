377382e5fab99d8a72bb2516431e19b575d8ce5d
maria larchenko <mashalarchenko16@gmail.com>
2020-06-19 16:47:53 +0300

    Updates

---
+++ precache-manifest.80fc95575d428b175445609880183db7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef46795829574b775025399811a58017",
+    "url": "/mashalarchenko.github.io/index.html"
+  },
+  {
+    "revision": "384c99d1965fa9e4545c",
+    "url": "/mashalarchenko.github.io/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "983fd9954c53d29421d3",
+    "url": "/mashalarchenko.github.io/static/css/main.0e2785a4.chunk.css"
+  },
+  {
+    "revision": "384c99d1965fa9e4545c",
+    "url": "/mashalarchenko.github.io/static/js/2.816c631f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mashalarchenko.github.io/static/js/2.816c631f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983fd9954c53d29421d3",
+    "url": "/mashalarchenko.github.io/static/js/main.74154de0.chunk.js"
+  },
+  {
+    "revision": "905979f52d70171ee14f",
+    "url": "/mashalarchenko.github.io/static/js/runtime-main.bfc5c7c0.js"
+  },
+  {
+    "revision": "5edbeeb90394792e34c6458e2ec767ca",
+    "url": "/mashalarchenko.github.io/static/media/1.5edbeeb9.jpg"
+  },
+  {
+    "revision": "60c22a8c0c70f2e3ef36da3a9c201c09",
+    "url": "/mashalarchenko.github.io/static/media/2.60c22a8c.jpg"
+  },
+  {
+    "revision": "04e2636f6619398c6fc0ecd0745b334f",
+    "url": "/mashalarchenko.github.io/static/media/3.04e2636f.jpg"
+  },
+  {
+    "revision": "e1a1e0dc24ecd8b793b1b351436d52b4",
+    "url": "/mashalarchenko.github.io/static/media/4.e1a1e0dc.jpg"
+  },
+  {
+    "revision": "6b8ae61793010ef87d734f519bd178c6",
+    "url": "/mashalarchenko.github.io/static/media/5.6b8ae617.jpg"
+  },
+  {
+    "revision": "ea98f6ba93597b4f6345f38a4f5298cb",
+    "url": "/mashalarchenko.github.io/static/media/7.ea98f6ba.jpg"
+  },
+  {
+    "revision": "60bdfe87e342f4f674f039e7ee468a40",
+    "url": "/mashalarchenko.github.io/static/media/8.60bdfe87.jpg"
+  },
+  {
+    "revision": "93548749d24705bc1a73a61b74bee922",
+    "url": "/mashalarchenko.github.io/static/media/9.93548749.jpg"
+  },
+  {
+    "revision": "2c012d6c02c810b474f6d41790b3244d",
+    "url": "/mashalarchenko.github.io/static/media/MilkyWay.2c012d6c.mp4"
+  }
+]);
